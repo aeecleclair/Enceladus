44a8d8a99a6d53f060e7a60c6a4d5e125a84ab0f
Lühmos <guillaume.foucher.pele@gmail.com>
2026-05-26 17:47:05 +0200

    misc

--- src/app/pmf/[locale]/login/page.tsx
+++ src/app/pmf/[locale]/login/page.tsx
@@ -9,35 +9,23 @@ export default function Login() {
   const t = useTranslations("pmf");
   const router = useRouter();
   return (
-    <div>
-      {t("login.title")}
+    <div className="flex flex-col items-center justify-center mt-20 gap-6">
+      <h1 className="text-3xl font-bold text-center">{t("login.title")}</h1>
       <MyECLButton subdomain="pmf" />
-      <span>{t("login.register")}</span>
-      <Button
-        variant="outline"
-        size="lg"
-        className="w-full m-auto"
-        onClick={() => {
-          const redirectUri =
-            process.env.NEXT_PUBLIC_BACKEND_URL + "/calypsso/register";
-          router.push(redirectUri);
-        }}
-      >
-        {t("login.student")}
-      </Button>
-      <Button
-        variant="outline"
-        size="lg"
-        className="w-full m-auto"
-        onClick={() => {
-          const redirectUri =
-            process.env.NEXT_PUBLIC_BACKEND_URL +
-            "/calypsso/register?external=true";
-          router.push(redirectUri);
-        }}
-      >
-        {t("login.alumni")}
-      </Button>
+      <p className="text-sm text-gray-300">{t("login.register")}</p>
+      <div className="flex justify-center">
+        <Button
+          variant="outline"
+          size="lg"
+          onClick={() => {
+            const redirectUri =
+              process.env.NEXT_PUBLIC_BACKEND_URL + "/calypsso/register";
+            router.push(redirectUri);
+          }}
+        >
+          {t("login.login")}
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/pmf/user/OfferCard.tsx
+++ src/components/pmf/user/OfferCard.tsx
@@ -1,20 +1,31 @@
 import { OfferSimple } from "@/api";
+import { useRouter } from "@/i18n/navigation";
+import { DrawingPinFilledIcon, LapTimerIcon } from "@radix-ui/react-icons";
+import { useTranslations } from "next-intl";
 
 interface OfferCardProps {
-    offer: OfferSimple;
+  offer: OfferSimple;
 }
 
-export default function OfferCard({ offer }:OfferCardProps) {
-
-    return (
-        <div className="border border-gray-200 rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-200">
-            <p className="font-bold text-3xl">{offer.title}</p>
-            <p className="">{offer.company_name}</p>
-            <p>{offer.duration} mois</p>
-            <p>{offer.location}</p>
-            <p>{offer.location_type}</p>
-            <p>{offer.offer_type}</p>
-            <p>{new Date(offer.start_date).toLocaleDateString()}</p>
-        </div>
-    );
+export default function OfferCard({ offer }: OfferCardProps) {
+  const router = useRouter();
+  const t = useTranslations("pmf");
+  return (
+    <div className="relative border border-gray-200 rounded-lg p-4">
+      <span className="absolute right-4 top-4 rounded-full bg-slate-100 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-slate-600">
+        {offer.offer_type}
+      </span>
+      <p className="font-semibold mb-3">{offer.company_name}</p>
+      <p className="font-bold text-3xl mb-3" onClick={() => router.push(`/offer?offerId=${offer.id}`)}>
+        {offer.title}
+      </p>
+      <div className="flex items-center gap-10">
+        <p className="flex items-center gap-2">
+          {<LapTimerIcon />} {offer.duration} {t("OfferPanel.months")}
+        </p>
+        <p className="flex items-center gap-2">{<DrawingPinFilledIcon />}{offer.location}</p>
+        <p>{new Date(offer.start_date).toLocaleDateString()}</p>
+      </div>
+    </div>
+  );
 };
