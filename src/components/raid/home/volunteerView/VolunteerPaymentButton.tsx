c4c108d9f1d49c4bd660e662ddc4d7b21ee1db7a
maximeroucher <maximeroucher@gmail.com>
2026-09-16 20:26:44 +0200

    fix: handling payment url error for volunteer

--- src/components/raid/home/volunteerView/VolunteerPaymentButton.tsx
+++ src/components/raid/home/volunteerView/VolunteerPaymentButton.tsx
@@ -28,56 +28,63 @@ export const VolunteerPaymentButton = () => {
   if (!isLoading && !!paymentUrl) {
     router.push(paymentUrl.url);
   }
+  const registeredPrice = !!price?.volunteer_price;
+
   const mustPay = !meVolunteer?.payment;
 
   return (
-    <>
-      <WarningDialog
-        isOpened={isOpened}
-        setIsOpened={setIsOpened}
-        isLoading={isLoading}
-        title={t("pay")}
-        description={
-          <div>
-            <div className="my-2 font-semibold">{t("summary")}</div>
-            <div className="space-y-2">
-              {mustPay && (
-                <div className="flex justify-between">
-                  <span>{t("participation")}</span>
-                  <span>{(price?.volunteer_price ?? 0) / 100} €</span>
-                </div>
-              )}
+    registeredPrice && (
+      <>
+        <WarningDialog
+          isOpened={isOpened}
+          setIsOpened={setIsOpened}
+          isLoading={isLoading}
+          title={t("pay")}
+          description={
+            <div>
+              <div className="my-2 font-semibold">{t("summary")}</div>
+              <div className="space-y-2">
+                {mustPay && (
+                  <div className="flex justify-between">
+                    <span>{t("participation")}</span>
+                    <span>{(price?.volunteer_price ?? 0) / 100} €</span>
+                  </div>
+                )}
+              </div>
+              <div className="mt-6 mb-2 font-semibold">
+                {t("paymentProviderInfo")}
+              </div>
+              <p>{t("helloAssoDescription")}</p>
             </div>
-            <div className="mt-6 mb-2 font-semibold">
-              {t("paymentProviderInfo")}
-            </div>
-            <p>{t("helloAssoDescription")}</p>
-          </div>
-        }
-        customButton={
-          <HelloAssoButton isLoading={isLoading} onClick={() => refetchUrl()} />
-        }
-      />
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <Button
-              className="col-span-4 ml-auto w-25"
-              disabled={!mustPay}
-              onClick={() => {
-                setIsOpened(true);
-              }}
-            >
-              {t("pay")}
-            </Button>
-          </TooltipTrigger>
-          {!mustPay && (
-            <TooltipContent>
-              <p>{t("fullyValidated")}</p>
-            </TooltipContent>
-          )}
-        </Tooltip>
-      </TooltipProvider>
-    </>
+          }
+          customButton={
+            <HelloAssoButton
+              isLoading={isLoading}
+              onClick={() => refetchUrl()}
+            />
+          }
+        />
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                className="col-span-4 ml-auto w-25"
+                disabled={!mustPay}
+                onClick={() => {
+                  setIsOpened(true);
+                }}
+              >
+                {t("pay")}
+              </Button>
+            </TooltipTrigger>
+            {!mustPay && (
+              <TooltipContent>
+                <p>{t("fullyValidated")}</p>
+              </TooltipContent>
+            )}
+          </Tooltip>
+        </TooltipProvider>
+      </>
+    )
   );
 };
