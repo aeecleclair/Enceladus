6c822ff7fba3c97d04737e57bca65e762e8d2024
maximeroucher <maximeroucher@gmail.com>
2026-09-21 23:05:27 +0200

    fix: handling volunteer payment redirection

--- src/components/raid/home/volunteerView/VolunteerPaymentButton.tsx
+++ src/components/raid/home/volunteerView/VolunteerPaymentButton.tsx
@@ -8,7 +8,7 @@ import { useVolunteerPaymentUrl } from "@/hooks/raid/useVolunteerPaymentUrl";
 import { useRouter } from "@/i18n/navigation";
 
 import { useTranslations } from "next-intl";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -25,15 +25,19 @@ export const VolunteerPaymentButton = () => {
   const { paymentUrl, isLoading, refetchUrl } = useVolunteerPaymentUrl();
   const [isOpened, setIsOpened] = useState(false);
   const router = useRouter();
-  if (!isLoading && !!paymentUrl) {
-    router.push(paymentUrl.url);
-  }
+  useEffect(() => {
+    if (!isLoading && !!paymentUrl) {
+      router.push(paymentUrl.url);
+    }
+  }, [isLoading, paymentUrl, router]);
   const registeredPrice = !!price?.volunteer_price;
+  const isCancelled = !!meVolunteer?.cancelled;
 
   const mustPay = !meVolunteer?.payment;
 
   return (
-    registeredPrice && (
+    registeredPrice &&
+    !isCancelled && (
       <>
         <WarningDialog
           isOpened={isOpened}
