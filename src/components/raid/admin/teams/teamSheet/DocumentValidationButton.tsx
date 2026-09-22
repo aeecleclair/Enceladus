38402f0318ffd1d25df061bbe0869a8a0898bab3
maximeroucher <maximeroucher@gmail.com>
2026-09-22 18:17:56 +0200

    fix: button layout

--- src/components/raid/admin/teams/teamSheet/DocumentValidationButton.tsx
+++ src/components/raid/admin/teams/teamSheet/DocumentValidationButton.tsx
@@ -5,17 +5,19 @@ import { useState } from "react";
 interface DocumentValidationButtonProps {
   validateDocument: (callback: () => void) => void;
   label: string;
+  className?: string;
 }
 
 export const DocumentValidationButton = ({
   validateDocument,
   label,
+  className = "flex-1",
 }: DocumentValidationButtonProps) => {
   const [isValidationLoading, setIsValidationLoading] = useState(false);
   return (
     <LoadingButton
       isLoading={isValidationLoading}
-      className="w-full"
+      className={className}
       onClick={() => {
         setIsValidationLoading(true);
         validateDocument(() => {
